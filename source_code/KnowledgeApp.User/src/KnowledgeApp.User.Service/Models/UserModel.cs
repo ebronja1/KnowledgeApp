16c92887e3c8a12d7f25e0c078b456e1a52ad0fe
ebronja1 <ebronja1@etf.unsa.ba>
2024-10-25 18:22:27 +0200

    add role

--- source_code/KnowledgeApp.User/src/KnowledgeApp.User.Service/Models/UserModel.cs
+++ source_code/KnowledgeApp.User/src/KnowledgeApp.User.Service/Models/UserModel.cs
@@ -8,5 +8,6 @@ namespace KnowledgeApp.User.Service.Models
         public Guid Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
