d1e2074e59d50425f2218fc5ff35f5ce70c6ab21
ebronja1 <ebronja1@etf.unsa.ba>
2024-11-01 17:46:59 +0100

    ...

--- source_code/KnowledgeApp.Paragraph/src/KnowledgeApp.Paragraph.Contracts/obj/Debug/net8.0/KnowledgeApp.Paragraph.Contracts.AssemblyInfo.cs
+++ source_code/KnowledgeApp.Paragraph/src/KnowledgeApp.Paragraph.Contracts/obj/Debug/net8.0/KnowledgeApp.Paragraph.Contracts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KnowledgeApp.Paragraph.Contracts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1-alpha+b9837d3143a817d76bfc1fc34ee435d4c6717a73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1-alpha+e86b901455bfb4efce00ef528506bd39a3ef7912")]
 [assembly: System.Reflection.AssemblyProductAttribute("KnowledgeApp.Paragraph.Contracts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KnowledgeApp.Paragraph.Contracts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
